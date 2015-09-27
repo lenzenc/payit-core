e7a709246902db0c93988ace7fc057f3cd9b3308
Craig Lenzen <craig.lenzen@concur.com>
2015-09-27 10:16:12 -0500

    [master]: Uncommented the User model code that didn't get updated after the Model/Id refactor

--- src/main/scala/com/payit/profile/models/User.scala
+++ src/main/scala/com/payit/profile/models/User.scala
@@ -1,20 +1,25 @@
 package com.payit.profile.models
 
 import com.mongodb.casbah.Imports.ObjectId
-import com.payit.components.core.models.{Timestamps, Id}
-//import com.payit.components.mongo.models.MongoModel
+import com.payit.components.core.models.Timestamps
+import com.payit.components.mongo.models.{MongoId, MongoModel}
 
-//case class UserId(value: ObjectId) extends Id(value)
-//
-//case class User(
-//  id: Option[UserId],
-//  firstName: String,
-//  mi: Option[String],
-//  lastName: String,
-//  loginId: String,
-//  password: String,
-//  email: String,
-//  customerId: CustomerId,
-//  isActive: Boolean = true,
-//  timestamps: Timestamps = Timestamps())
-//extends MongoModel[User]
+case class UserId(value: ObjectId) extends MongoId
+
+case class User(
+  id: Option[UserId],
+  firstName: String,
+  mi: Option[String],
+  lastName: String,
+  loginId: String,
+  password: String,
+  email: String,
+  customerId: CustomerId,
+  isActive: Boolean = true,
+  timestamps: Timestamps = Timestamps())
+extends MongoModel[UserId, User]
+{
+
+  def withId(idValue: ObjectId) = copy(id = Some(UserId(idValue)))
+
+}
