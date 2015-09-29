8b9896275e5025fbd79758af7621946d776c1473
Craig Lenzen <craig.lenzen@concur.com>
2015-09-29 14:08:46 -0500

    [master]: Fixed a bug where the mongo config name was being used as the source in the mongo connection string instead of the actual name of the db

--- src/main/scala/com/payit/components/mongo/MongoDBSupport.scala
+++ src/main/scala/com/payit/components/mongo/MongoDBSupport.scala
@@ -5,12 +5,12 @@ import com.payit.components.core.Configuration
 
 trait MongoDBSupport {
 
-  def mongoDB: MongoDB = client(config.getString(s"mongo.$dbConfigName.dbname").getOrElse(
-    sys.error(s"Unable to find dbname configuration for config: ${s"mongo.$dbConfigName.dbname"}"))
-  )
+  def mongoDB: MongoDB = client(dbName)
 
   protected def config: Configuration
   protected def dbConfigName: String
+  protected def dbName: String = config.getString(s"mongo.$dbConfigName.dbname").getOrElse(
+    sys.error(s"Unable to find dbname configuration for config: ${s"mongo.$dbConfigName.dbname"}"))
   protected lazy val client = buildClient
 
   protected def buildClient: MongoClient = {
@@ -32,7 +32,7 @@ trait MongoDBSupport {
     val password = config.getString(s"mongo.$dbConfigName.password")
     if (username.isDefined && password.isDefined) List(MongoCredential.createCredential(
       username.get,
-      dbConfigName,
+      dbName,
       password.get.toArray))
     else List.empty[MongoCredential]
   }
