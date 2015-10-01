be37ddafbbc064b916f9625d20a12614dd6a7cb1
Craig Lenzen <craig.lenzen@concur.com>
2015-10-01 13:58:48 -0500

    [validations]: Added a simple validation DSL that allows for the definition of validation rules for a given object

--- src/main/scala/com/payit/components/validation/package.scala
+++ src/main/scala/com/payit/components/validation/package.scala
@@ -1,7 +1,25 @@
 package com.payit.components
 
-import com.payit.components.validation.rules.{StringRules, OrderingRules, GeneralRules}
+import com.payit.components.validation.rules.{ValidationRule, StringRules, OrderingRules, GeneralRules}
 
 package object validation extends GeneralRules with OrderingRules with StringRules {
+
+  def validator[T](key: String, validationRuleSets: ValidationRuleSet[T, _]*): Validator[T] = new Validator[T] {
+    override val parentKey = ParentKey(key)
+    val ruleSets = validationRuleSets
+  }
+
+  case class ValidationProp[T, V](key: String, prop: (T) => V)
+
+  implicit class RuleOpts[T, V](validationProp: ValidationProp[T, V]) {
+    def is(validationRules: ValidationRule[V]*): ValidationRuleSet[T, V] = new ValidationRuleSet[T, V] {
+      val parentKey = ParentKey()
+      val key = validationProp.key
+      val mapper = validationProp.prop
+      val rules = validationRules
+    }
+  }
+
+  def prop[T, V](key: String, prop: (T) => V) = ValidationProp[T, V](key, prop)
 
 }
