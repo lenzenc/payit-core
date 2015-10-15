2e51b88e3bd7d7c8d4ef1e8960240168582d8980
Craig Lenzen <lenzenc@gmail.com>
2015-10-15 18:04:22 -0500

    [convert-to-scala-validations]: Removed all the validations framework as it is now part of a seperate project

--- src/main/scala/com/payit/components/validation/ParentKey.scala
+++
@@ -1,5 +0,0 @@
-package com.payit.components.validation
-
-case class ParentKey(keys: String*) {
-  override def toString() = keys.mkString("->")
-}
--- src/main/scala/com/payit/components/validation/ValidationFailure.scala
+++
@@ -1,8 +0,0 @@
-package com.payit.components.validation
-
-case class ValidationFailure(
-  parentKey: ParentKey,
-  key: String,
-  ruleKey: String,
-  message: String,
-  params: Seq[Any] = Seq.empty)
